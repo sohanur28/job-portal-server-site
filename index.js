bab3e5b54f85e56cb532924e253071d2a635710e
sohanur28 <gsrahman28@gmail.com>
2023-07-24 22:24:36 +0600

    freshJob

--- index.js
+++ index.js
@@ -26,6 +26,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const fresherJobCollection = client.db("jobportalDb").collection("freshjob");
+    const experiencedJobCollection = client.db("jobportalDb").collection("expjob");
+
+    app.get('/freshjob', async(req, res) => {
+        const result = await fresherJobCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get('/expjob', async(req, res) => {
+        const result = await experiencedJobCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
